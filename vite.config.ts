3508ece0c05cac016bed8e18087fd0c67c22cd98
Egor Gavrilov <hellguz@gmail.com>
2026-04-14 15:44:19 +0200

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wolfsburg-web/',
+  base: process.env.VITE_BASE_PATH || '/wolfsburg-web/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
